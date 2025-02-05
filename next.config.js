bf6bcafad8612a72d46e414da7bf57e007a5e4f6
vladilen11 <vladilenaksana@163.com>
2025-02-06 02:50:55 +0800

    support local sign and format code

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
 module.exports = {
-    experimental: {
-        // outputStandalone: true,
-        ignoreDuringBuilds: true,
-    },
-    staticPageGenerationTimeout: 1000,
-    eslint: {
-        dirs: ['src'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-    },
-}
+  experimental: {
+    // outputStandalone: true,
+    ignoreDuringBuilds: true,
+  },
+  staticPageGenerationTimeout: 1000,
+  eslint: {
+    dirs: ['src'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+  },
+};
--- src/chain/config.ts
+++ src/chain/config.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0x05a424e185c8ea297a05ef14dfd58db26a9e17d233aa3d097317ec2883061d57'
-export const SCHEMA_ID = '0x6f049c9b4b8667ef8d7ffec5ff402debc3f5c91f73fc6d7c17775352f00b923d'
+export const PACKAGE_ID = '0x05a424e185c8ea297a05ef14dfd58db26a9e17d233aa3d097317ec2883061d57';
+export const SCHEMA_ID = '0x6f049c9b4b8667ef8d7ffec5ff402debc3f5c91f73fc6d7c17775352f00b923d';
