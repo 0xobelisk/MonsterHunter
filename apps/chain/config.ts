345a6b0d639f837251f8f06d488671f7085f2711
vladilen11 <vladilenaksana@163.com>
2025-03-28 19:15:16 +0800

    Some nits.

--- apps/chain/config.ts
+++
@@ -1,5 +0,0 @@
-type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
-
-export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0x36f628b19671f60f535231a05a780e07795998ac9c6f80392beb15e424860c6c'
-export const SCHEMA_ID = '0x8ca26538c4a5a3f884202e67b99cbd735a0a227b3b8456904de34764999ae3e7'
